685a34a3c4396e38e0199a232011b2156674f5ae
Arno Granier <arno.granier@unibe.ch>
2022-06-20 16:35:32 +0200

    comments plot util

--- plotutils.py
+++ plotutils.py
@@ -5,6 +5,17 @@ import numpy as np
 from matplotlib.patches import ConnectionPatch
 
 def connect_subplots(ax1, ax2, arrowstyle="-|>,head_width=1,head_length=1", **kwargs):
+    """Helper function to draw arrows between subplots.
+
+    Args:
+        ax1 (matplotlib.axes.Axes): ax to draw the arrow from.
+        ax2 (matplotlib.axes.Axes): ax to draw the arrow to.
+        arrowstyle (str, optional): matplotlib arrowstyle. Defaults to "-|>,head_width=1,head_length=1".
+        **kwargs: Forwarded to matplotlib.patches.ConnectionPatch.
+
+    Returns:
+        matplotlib.patches.ConnectionPatch: the added patch artist.
+    """
     ycenter0 = sum(ax1.get_ylim())/2
     ycenter1 = sum(ax2.get_ylim())/2
     xright0 = ax1.get_xlim()[1]
@@ -14,6 +25,18 @@ def connect_subplots(ax1, ax2, arrowstyle="-|>,head_width=1,head_length=1", **kw
                                           lw=3, arrowstyle=arrowstyle, fc='black', **kwargs))
 
 def gaussian2dplot(ax, m, cloud, points=None, vs=None):
+    """Plot a gaussian cloud with confidence ellipse, a potential set of two points and principal components
+
+    Args:
+        ax (matplotlib.axes.Axes): ax to draw to
+        m (array-like, shape (2,)): mean vector
+        cloud (array-like, shape (n, 2)): point cloud
+        points (array-like, shape (2, 2) optional): set of two points to plot. Defaults to None.
+        vs (array-like, shape (2, 2), optional): principal components. Defaults to None.
+
+    Returns:
+        matplotlib.axes.Axes: the ax
+    """
     ax.scatter(cloud[:,0], cloud[:,1], color='gray', s=10)
     confidence_ellipse(cloud[:,0], cloud[:,1], ax, edgecolor='red', lw=3)
     ax.scatter(m[0], m[1], color='red', s=200)
@@ -29,26 +52,21 @@ def gaussian2dplot(ax, m, cloud, points=None, vs=None):
 
 # Confidence ellipse helper function from https://matplotlib.org/stable/gallery/statistics/confidence_ellipse.html
 def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
-    """
-    Create a plot of the covariance confidence ellipse of *x* and *y*.
+    """Create a plot of the covariance confidence ellipse of *x* and *y*.
+    
+    Args:
+        x (array-like, shape (n, )): Input data.
+        y (array-like, shape (n, )): Input data.
+        ax (matplotlib.axes.Axes): The axes object to draw the ellipse into.
+        n_std (float, optional): The number of standard deviations to determine the ellipse's radiuses. Defaults to 3.0.
+        facecolor (str, optional): Forwarded to `~matplotlib.patches.Ellipse`. Defaults to 'none'.
+        **kwargs: Forwarded to `~matplotlib.patches.Ellipse`.
 
-    Parameters
-    ----------
-    x, y : array-like, shape (n, )
-        Input data.
+    Raises:
+        ValueError: size check.
 
-    ax : matplotlib.axes.Axes
-        The axes object to draw the ellipse into.
-
-    n_std : float
-        The number of standard deviations to determine the ellipse's radiuses.
-
-    **kwargs
-        Forwarded to `~matplotlib.patches.Ellipse`
-
-    Returns
-    -------
-    matplotlib.patches.Ellipse
+    Returns:
+        matplotlib.patches.Ellipse: the drawn ellipse.
     """
     if x.size != y.size:
         raise ValueError("x and y must be the same size")
